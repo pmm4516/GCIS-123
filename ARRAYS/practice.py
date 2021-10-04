f06d9cd761dd6364372e199347940b184d95ada2
Patrick Marchione <pmm4516@rit.edu>
2021-10-04 08:45:32 -0400

    Stubbed out new Array and Recursion problem files

---
+++ ARRAYS/practice.py
@@ -0,0 +1,17 @@
+"""
+Author: Patrick Marchione
+
+Arrays and shit
+
+Goal of this
+    - have the user input what their fav number is from 0 - 100
+    - have the user input what their least fav number is from 0 - 100
+    - build two random arrays filled with random numbers
+    - search through both arrays using linear search looking for their
+    fav number in first array and least fav in second array
+    - time both searches and see which is quicker
+
+Write some TDD tests for it for extra practice
+"""
+import arrays
+import time
---
+++ RECURSION/practice.py
@@ -0,0 +1,13 @@
+"""
+Author: Patrick Marchione
+
+Goal: Have the user enter a number
+From that number, build a loop to determine how many multiples of 7 are less than that number
+Then transform that loop in a recursive function that does the same thing
+
+Have the user enter their favorite fruit
+Build a loop to reverse the string
+Then transform that loop into a recursive function
+
+Write some TDD tests for it for extra practice
+"""
