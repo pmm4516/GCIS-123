890eea3dba4a05d6640c4b98f0258ba766b2a544
Patrick Marchione <pmm4516@rit.edu>
2021-11-29 16:18:30 -0500

    kobe2

---
+++ EXAM_REVIEW/UNIT 1-5/review_without_answer.py
@@ -0,0 +1,29 @@
+"""
+Final Practical Review 1
+Units 1-5
+"""
+
+import turtle
+
+# Question 1
+# Enter your name and print it out
+def name():
+    pass
+
+# Question 2
+# Now draw a square
+def tt():
+    pass
+
+# Question 3
+# Read through the declaration of independence and grab every vowel and count em and write it to another file
+def independence():
+    pass
+
+
+def main():
+    name()
+    tt()
+    independence()
+
+main()
